3d35ed90aeb56cb3dc3ac824ce4c1ba8e1f7fda7
tthaav@utu.fi <tthaav@utu.fi@9dad6250-64d0-347a-460f-6cd84095dec1>
2011-08-30 22:02:45 +0000

    Sain tehtyä käyttäjäprofiililaajennuksen (UserProfile-model)... Rekisteröinti kysyy nyt edellisten tietojen lisäksi myös etu- ja sukunimeä, jotka sitten kirjautumisen jälkeen näkyvät pääsivulla. Jouduin tyhjäämään käyttäjätietokannan jotenka rekisteröitykää uusiksi testausmielessä, käyttäjälista löytyy urlista /UserList/ ... siellä ei ole muuta toiminnallisuutta vielä, tein vain listaviewin ja controllerin jotta näkyy miten userprofilea käytetään. Huomiseen!

---
+++ trunk/bugtracker/bugtracker/Models/UserList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace bugtracker.Models
+{
+    public class UserList
+    {
+        public MembershipUserCollection Users { get; set; }
+        public IEnumerable<UserProfile> UserProfiles { set; get; }
+    }
+}
---
+++ trunk/bugtracker/bugtracker/Models/UserProfile.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Profile;
+using System.ComponentModel.DataAnnotations;
+
+namespace bugtracker.Models
+{
+    public class UserProfile : ProfileBase
+    {
+        [Display(Name = "First Name")]
+        public virtual string FirstName
+        {
+            get
+            {
+                return (this.GetPropertyValue("FirstName").ToString());
+            }
+            set
+            {
+                this.SetPropertyValue("FirstName", value);
+            }
+        }
+
+        [Display(Name = "Last Name")]
+        public virtual string LastName
+        {
+            get
+            {
+                return (this.GetPropertyValue("LastName").ToString());
+            }
+            set
+            {
+                this.SetPropertyValue("LastName", value);
+            }
+        }
+
+        [Display(Name = "UserID")]
+        public virtual int UserID
+        {
+            get
+            {
+                return ((int)(this.GetPropertyValue("UserID")));
+            }
+            set
+            {
+                this.SetPropertyValue("UserID", value);
+            }
+        }
+
+        public static UserProfile GetProfile(string username)
+        {
+            return Create(username) as UserProfile;
+        }
+    }
+}
