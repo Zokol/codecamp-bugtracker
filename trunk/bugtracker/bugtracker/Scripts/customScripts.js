6bcb19c65d30dbfa9f37fa63bd37215fda8fa4a4
qwertyjelly@gmail.com <qwertyjelly@gmail.com@9dad6250-64d0-347a-460f-6cd84095dec1>
2011-09-01 08:22:33 +0000

    separated some js, bug/details made prettier

---
+++ trunk/bugtracker/bugtracker/Scripts/customScripts.js
@@ -0,0 +1,31 @@
+﻿function showModalPopup(s) {
+
+        var maskHeight = $(document).height();
+        var maskWidth = $(window).width();
+
+        //Set height and width to mask to fill up the whole screen
+        $('#mask').css({ 'width': maskWidth, 'height': maskHeight });
+
+        //transition effect     
+        $('#mask').fadeIn(1000);
+        $('#mask').fadeTo("slow", 0.8);
+
+        //Get the window height and width
+        var winH = $(window).height();
+        var winW = $(window).width();
+
+        loadPartialView(s, 'dialog');
+
+        //Set the popup window to center
+        $('#dialog').css('top', '10px');
+        $('#dialog').css('left', winW / 2 - $('#dialog').width() / 2);
+
+
+        //transition effect
+        $('#dialog').fadeIn(2000);
+
+    };
+
+function closeModalPopup() {
+        $('#mask, .window').hide();
+    };
