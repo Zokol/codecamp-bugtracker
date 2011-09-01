3a87e64ff3a2bcc49902b18efe69a320ceff1bdd
rahaav@utu.fi <rahaav@utu.fi@9dad6250-64d0-347a-460f-6cd84095dec1>
2011-09-01 16:05:54 +0000

    some data restrictions to bug-model

--- trunk/bugtracker/bugtracker/Models/Bug.cs
+++ trunk/bugtracker/bugtracker/Models/Bug.cs
@@ -4,15 +4,37 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity;
 using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace bugtracker.Models
 {
     public class Bug
     {
         public int ID { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Title")]
         public string Title { get; set; }
+
+        [Required]
+        [StringLength(400, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 20)]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Description")]
         public string Description { get; set; }
+
+
+        [Required]
+        [Range(0, 5)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Criticality")]
         public int Criticality { get; set; }
+
+        [Required]
+        [Range(0, 5)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Criticality")]
         public int PriorityID { get; set; }
         public int StatusID { get; set; }
         public int BugTypeID { get; set; }
